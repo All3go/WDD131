7787052ac0677f3a8fa3aebee6d8185dacef0e92
Austin Linford <alllego321@gmail.com>
2025-07-03 22:08:17 -0600

    Finished

--- recipes/main.js
+++ recipes/main.js
@@ -1,5 +1,39 @@
-import  recipes  from './recipes.js';
+import recipes from './recipes.js';
 import renderRecipeGallery from './recipecard_mod.js';
 
-const gallery = document.getElementById('recipe-gallery');
-renderRecipeGallery(recipes, gallery);
+function getRandomListEntry(list) {
+  const index = Math.floor(Math.random() * list.length);
+  return list[index];
+}
+
+function init() {
+  const gallery = document.getElementById('recipe-gallery');
+  const randomRecipe = getRandomListEntry(recipes);
+  renderRecipeGallery([randomRecipe], gallery);
+}
+
+init();
+
+function filterRecipes(query) {
+  query = query.toLowerCase();
+  return recipes
+    .filter(recipe =>
+      recipe.name.toLowerCase().includes(query) ||
+      recipe.description.toLowerCase().includes(query) ||
+      recipe.recipeIngredient.find(ing => ing.toLowerCase().includes(query)) ||
+      recipe.tags.find(tag => tag.toLowerCase().includes(query))
+    )
+    .sort((a, b) => a.name.localeCompare(b.name));
+}
+
+function searchHandler(event) {
+  event.preventDefault();
+  const input = document.querySelector('#search-form input[type="search"]');
+  const query = input.value;
+  const gallery = document.getElementById('recipe-gallery');
+  gallery.innerHTML = ''; // clear old recipes
+  const filtered = filterRecipes(query);
+  renderRecipeGallery(filtered, gallery);
+}
+
+document.getElementById('search-form').addEventListener('submit', searchHandler);
